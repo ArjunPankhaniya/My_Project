48b20d92040ddb0bec268216d45793476e2b30b0
ArjunPankhaniya <71897061+ArjunPankhaniya@users.noreply.github.com>
2022-05-28 13:01:11 +0530

    Open A Bank Account Using Python
    Write a menu driven program featuring banking facilities. Include following
    features in the application: 
    1. To open a bank account 
    2. To deposit amount 
    3. To withdraw amount (keeping minimum balance of Rs. 500/-) 
    4. To display account information

---
+++ Python Micro Project.py
@@ -0,0 +1,35 @@
+banner=('====================\nwelcome to your bank\n====================\n')
+print(banner)
+name=str(input('Enter Your Name:'))
+birthdate=input('Enter Your Birthdate:')
+mobile_number=int(input('Enter Your Mobile Number:'))
+print('\n============================\nAccount Successfully Created\n============================\n')
+total=0
+print('Current Balance:',total)
+while True:
+    input_value = int(input('\nMenu\n1. To Deposit\n2. To Withdraw\n3. To See Your Balance\n4. To Display Account Information\n\nEnter your choice: '))
+    if input_value == 1:
+        deposit=int(input('Enter Amount To Deposit:'))
+        total += deposit
+        print('Deposited Amount:',deposit)
+        print('Current Balance:',total)
+    elif input_value == 2:
+        withdraw=int(input('Enter Amount To Withdraw:'))
+        min_balance=float('500')
+        min_balance = total - min_balance
+        if withdraw>min_balance:
+            print('\nInsufficient Balance!!\nBalance Must Be Rs. 500\n')
+        else:
+            total -=withdraw
+            print('\nWithdrawed Amount:',withdraw)
+            print('Current Balance:',total)
+    elif input_value == 3:
+        print('Current Balance:',total)
+    elif input_value == 4:
+        print('Name:',name)
+        print('Birthdate:',birthdate)
+        print('Mobile Number:',mobile_number)
+        print('Current Balance:',total)
+    else:
+        break
+print('\nFinished.')
