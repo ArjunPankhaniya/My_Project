1ce583f4498fb67decfe5ad91e136cedbf359000
ArjunPankhaniya <71897061+ArjunPankhaniya@users.noreply.github.com>
2022-06-04 11:57:14 +0530

    Update: Exception Handling In input mobile_number
    Updating: Exception Handling During Entering Wrong Input Of Mobile Number.

--- Python Micro Project.py
+++ Python Micro Project.py
@@ -1,11 +1,15 @@
 banner=('====================\nwelcome to your bank\n====================\n')
 print(banner)
-name=str(input('Enter Your Name:'))
-birthdate=input('Enter Your Birthdate:')
-mobile_number=int(input('Enter Your Mobile Number:'))
+try:
+    name=input('Enter Your Name:')
+    birthdate=input('Enter Your Birthdate:')
+    mobile_number=int(input('Enter Your Mobile Number:'))
+except ValueError:
+    print('\n*Enter Correct Mobile Number*')
 print('\n============================\nAccount Successfully Created\n============================\n')
 total=0
 print('Current Balance:',total)
+flag=0
 while True:
     input_value = int(input('\nMenu\n1. To Deposit\n2. To Withdraw\n3. To See Your Balance\n4. To Display Account Information\n\nEnter your choice: '))
     if input_value == 1:
@@ -26,10 +30,14 @@ while True:
     elif input_value == 3:
         print('Current Balance:',total)
     elif input_value == 4:
-        print('Name:',name)
-        print('Birthdate:',birthdate)
-        print('Mobile Number:',mobile_number)
-        print('Current Balance:',total)
+        try:
+            print('Name:',name)
+            print('Birthdate:',birthdate)
+            print('Mobile Number:',mobile_number)
+            print('Current Balance:',total)
+        except NameError:
+            print('\n*Enter Correct Mobile Number*')
     else:
+        print('\nTerminating season....')
         break
 print('\nFinished.')
